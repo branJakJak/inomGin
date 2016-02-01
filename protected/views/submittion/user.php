000bfeca70bfd7fc7c52e4867655611cadbc93be
kebyang <kelvo_link@yahoo.com>
2016-02-01 21:13:04 +0800

    asd

--- protected/views/submittion/user.php
+++ protected/views/submittion/user.php
@@ -6,7 +6,7 @@
         
 <?php
     $this->beginWidget('zii.widgets.CPortlet', array(
-        'title'=>'Leads submitted by '.echo $userModel->id;,
+        'title'=>'Leads submitted by '.$userModel->id;,
     ));
 ?>
     <?php if (Yii::app()->user->name === 'admin'): ?>
