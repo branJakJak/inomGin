f54b035a1b4696693293e31516c1937325fe4b1b
kek <top_kek@kek.com>
2016-02-08 18:44:01 +0800

    remove create and update for contorller

--- protected/controllers/MainLeadController.php
+++ protected/controllers/MainLeadController.php
@@ -27,15 +27,15 @@ class MainLeadController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
+			array('allow',
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+			// array('allow',
+			// 	'actions'=>array('create','update'),
+			// 	'users'=>array('@'),
+			// ),
+			array('allow', 
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
 			),
